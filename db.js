3692d474c21156df02a6a5e0a8695b1ce2ac2f49
Ali Rida Hussein <aliridahussein@outlook.com>
2025-04-13 17:14:33 +0300

    change db in db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 // Update these values with your actual PostgreSQL credentials
 const pool = new Pool({
   user: 'postgres',           // default user is usually 'postgres'
-  host: 'localhost',          // if running PostgreSQL locally
+  host: 'db',          // if running PostgreSQL locally
   database: 'taskdb',         // the DB you created earlier
   password: 'P@ssw0rd',   // the password you set during install
   port: 5432,                 // default PostgreSQL port
